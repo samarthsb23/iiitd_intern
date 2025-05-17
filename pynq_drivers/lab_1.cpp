7c0e49e7ef8f1c103ee33de586c005b2d8d8568a
Samarth Sharma B. <143651416+samarthsb23@users.noreply.github.com>
2025-05-17 11:35:16 +0530

    Add files via upload

---
+++ pynq_drivers/lab_1.cpp
@@ -0,0 +1,5 @@
+#include "lab_1.h"
+
+void lab_1(data_t A, data_t B, data_t C, data_t &Y) {
+	Y = A + 2*B - C;
+}
---
+++ pynq_drivers/lab_1.h
@@ -0,0 +1,10 @@
+#ifndef _LAB_1_ //if not defined
+#define _LAB_1_
+
+#include <ap_int.h>
+
+typedef ap_int<32> data_t;
+
+void lab_1(data_t A, data_t B, data_t C, data_t &Y);
+
+#endif
